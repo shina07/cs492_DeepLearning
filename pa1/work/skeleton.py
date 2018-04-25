9c2845caca92f401dc53049a27477e226d35837b
shina07 <limtsoo@kaist.ac.kr>
2018-04-25 15:06:43 +0000

    Implement basic 3 layer feedforward network

--- pa1/work/skeleton.py
+++ pa1/work/skeleton.py
@@ -1,23 +1,27 @@
+import sys
 import tensorflow as tf
 import numpy as np
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
-def custom_model_fn(features, labels, mode):
+def custom_model_fn(features, labels, mode, params):
     """Model function for PA1"""
 
-    # Write your custom layer
+    layers = params["layers"]
+    learning_rate = params["learning_rate"]
+    units = params["units"]
 
     # Input Layer
-    input_layer = tf.reshape(features["x"], [-1, 28, 28, 1]) # You also can use 1 x 784 vector
+    input_layer = tf.reshape(features["x"], [-1, 784]) # You also can use 1 x 784 vector
 
-    # ....
-    # ....
-    # ....
-    # ....
+    # first_hidden_layer = tf.layers.dense (inputs = input_layer, units = 10)
+    first_hidden_layer = tf.layers.dense (inputs = input_layer, units = 2000, activation = tf.nn.relu)
+    
+    # second_hidden_layer = tf.layers.dense (inputs = first_hidden_layer, units = 10)
+    second_hidden_layer = tf.layers.dense (inputs = first_hidden_layer, units = 2000, activation = tf.nn.relu)
 
     # Output logits Layer
-    logits = tf.layers.dense(inputs="your custom layer", units=10)
+    logits = tf.layers.dense(inputs = second_hidden_layer, units = 10)
 
     predictions = {
       # Generate predictions (for PREDICT and EVAL mode)
@@ -31,13 +35,15 @@ def custom_model_fn(features, labels, mode):
     if mode == tf.estimator.ModeKeys.PREDICT:
         return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
 
-    # Select your loss and optimizer from tensorflow API
-    # Calculate Loss (for both TRAIN and EVAL modes)
-    loss = tf.losses."custom loss function" # Refer to tf.losses
+    loss = tf.losses.sparse_softmax_cross_entropy (
+        labels,
+        logits
+    )
 
     # Configure the Training Op (for TRAIN mode)
     if mode == tf.estimator.ModeKeys.TRAIN:
-        optimizer = tf.train."custom optimizer" # Refer to tf.train
+        # optimizer = tf.train."custom optimizer" # Refer to tf.train
+        optimizer = tf.train.GradientDescentOptimizer (learning_rate) # Learning Rate
         train_op = optimizer.minimize(loss=loss, global_step=tf.train.get_global_step())
         return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
 
@@ -47,10 +53,25 @@ def custom_model_fn(features, labels, mode):
 
 
 if __name__ == '__main__':
+    argv = sys.argv[1:]
+
+    # layers = 3
+    # units = 100
+    # batch_size = 100
+    # learning_rate = 0.1
+    # steps = 20000
+    
+    layers = argv[0]
+    units = argv[1]
+    batch_size = argv[2]
+    learning_rate = argv[3]
+    steps = argv[4]
+
+
     # Write your dataset path
-    dataset_train = np.load('path/to/train_data')
-    dataset_eval =  np.load('path/to/valid_data')
-    test_data =  np.load('path/to/test_data')
+    dataset_train = np.load('cs492c_assignment1_data/train.npy')
+    dataset_eval =  np.load('cs492c_assignment1_data/valid.npy')
+    test_data =  np.load('cs492c_assignment1_data/test.npy')
 
     train_data = dataset_train[:,:784]
     train_labels = dataset_train[:,784].astype(np.int32)
@@ -58,7 +79,8 @@ if __name__ == '__main__':
     eval_labels = dataset_eval[:,784].astype(np.int32)
 
     # Save model and checkpoint
-    classifier = tf.estimator.Estimator(model_fn=custom_model_fn, model_dir="./model")
+    model_params = {:"layers" : layers, "units" : units, "learning_rate" : learning_rate}
+    classifier = tf.estimator.Estimator(model_fn=custom_model_fn, model_dir="./model", params = model_params)
 
     # Set up logging for predictions
     tensors_to_log = {"probabilities": "softmax_tensor"}
@@ -66,8 +88,8 @@ if __name__ == '__main__':
 
     # Train the model. You can train your model with specific batch size and epoches
     train_input = tf.estimator.inputs.numpy_input_fn(x={"x": train_data},
-        y=train_labels, batch_size=100, num_epochs=None, shuffle=True)
-    classifier.train(input_fn=train_input, steps=20000, hooks=[logging_hook])
+        y=train_labels, batch_size=batch_size, num_epochs=None, shuffle=True)
+    classifier.train(input_fn=train_input, steps=steps, hooks=[logging_hook])
 
     # Eval the model. You can evaluate your trained model with validation data
     eval_input = tf.estimator.inputs.numpy_input_fn(x={"x": eval_data},
@@ -79,7 +101,9 @@ if __name__ == '__main__':
     # Predict the test dataset
     pred_input = tf.estimator.inputs.numpy_input_fn(x={"x": test_data}, shuffle=False)
     pred_results = classifier.predict(input_fn=pred_input)
-    result = np.asarray([x.values()[1] for x in list(pred_results)])
+    # result = np.asarray([x.values()[1] for x in list(pred_results)])
+    pred_list = list (pred_results)
+    result = np.asarray ([list (x.values())[1] for x in pred_list])
     ## ----------------------------------------- ##
 
-    np.save('your_studentID.npy', result)
+    np.save('20130538.npy', result)
