b735edcc385484fb5611d8bb522863eb070eb4c0
shina07 <limtsoo@naver.com>
2020-10-08 15:25:51 +0900

    pa2 updated. pa2_extra added

--- pa2/work/skeleton.py
+++ pa2/work/skeleton.py
@@ -5,6 +5,7 @@ import numpy as np
 tf.logging.set_verbosity(tf.logging.INFO)
 
 TRAIN_DATA_PATH = "data/train.npy"
+TRAIN_DATA_AUGMENTED_PATH = "data/train_augmented_rotation_x20.npy"
 EVAL_DATA_PATH = "data/valid.npy"
 TEST_DATA_PATH = "data/test.npy"
 
@@ -22,35 +23,44 @@ def custom_model_fn(features, labels, mode, params):
         is_training = True
 
     # Input Layer
-    # input_layer = tf.reshape(features["x"], [-1, 784]) # You also can use 1 x 784 vector
     input_layer = tf.reshape (features["x"], [-1, 28, 28, 1])
 
-    # Hidden Layers for depth 3
-    # hidden_layer = tf.layers.dense (inputs = input_layer, units = units, activation = tf.nn.relu)
-    # hidden_layer = tf.layers.dropout (inputs = hidden_layer, rate = dropout_rate, training = is_training)
-    # hidden_layer = tf.layers.dense (inputs = hidden_layer, units = units, activation = tf.nn.relu)
-    # hidden_layer = tf.layers.dropout (inputs = hidden_layer, rate = dropout_rate, training = is_training)
+    # Convolutional-Network-3
+    if (depth == 3):
+        conv_layer = tf.layers.conv2d (inputs = input_layer, filters = 32, kernel_size = [filter_size, filter_size], padding = "same", activation = tf.nn.relu)
+        conv_layer = tf.layers.conv2d (inputs = conv_layer, filters = 32, kernel_size = [filter_size, filter_size], padding = "same", activation = tf.nn.relu)
+        pooling = tf.layers.max_pooling2d (inputs = conv_layer, pool_size = [2, 2], strides = 2)
+        pooling_flat = tf.reshape (pooling, [-1, 14 * 14 * 32])
 
-    # Convolutional Layer #1
-    conv_layer = tf.layers.conv2d (inputs = input_layer, filters = 32, kernel_size = [5, 5], padding = "same", activation = tf.nn.relu)
-    conv_layer = tf.layers.conv2d (inputs = conv_layer, filters = 32, kernel_size = [5, 5], padding = "same", activation = tf.nn.relu)
-    pooling = tf.layers.max_pooling2d (inputs = conv_layer, pool_size = [2, 2], strides = 2)
-    pooling_flat = tf.reshape (pooling, [-1, 14 * 14 * 32])
+    if (depth == 5):
+        conv_layer = tf.layers.conv2d (inputs = input_layer, filters = 32, kernel_size = [filter_size, filter_size], padding = "same", activation = tf.nn.relu)
+        conv_layer = tf.layers.conv2d (inputs = conv_layer, filters = 32, kernel_size = [filter_size, filter_size], padding = "same", activation = tf.nn.relu)
+        pooling = tf.layers.max_pooling2d (inputs = conv_layer, pool_size = [2, 2], strides = 2)
+        dropout = tf.layers.dropout (inputs = pooling, rate = dropout_rate, training = is_training)
 
-    if (depth >= 5):
-        conv_layer = tf.layers.conv2d (inputs = pooling, filters = 64, kernel_size = [5, 5], padding = "same", activation = tf.nn.relu)
-        pooling = tf.layers.conv2d (inputs = conv_layer, filters = 64, kernel_size = [5, 5], padding = "same", activation = tf.nn.relu)
+        conv_layer = tf.layers.conv2d (inputs = dropout, filters = 64, kernel_size = [filter_size, filter_size], padding = "same", activation = tf.nn.relu)
+        conv_layer = tf.layers.conv2d (inputs = conv_layer, filters = 64, kernel_size = [filter_size, filter_size], padding = "same", activation = tf.nn.relu)
         pooling = tf.layers.max_pooling2d (inputs = conv_layer, pool_size = [2, 2], strides = 2)
         pooling_flat = tf.reshape (pooling, [-1, 7 * 7 * 64])
 
     if (depth == 7):
-        conv_layer = tf.layers.conv2d (inputs = pooling, filters = 128, kernel_size = [5, 5], padding = "same", activation = tf.nn.relu)
-        pooling = tf.layers.conv2d (inputs = conv_layer, filters = 128, kernel_size = [5, 5], padding = "same", activation = tf.nn.relu)
+        conv_layer = tf.layers.conv2d (inputs = input_layer, filters = 32, kernel_size = [filter_size, filter_size], padding = "same", activation = tf.nn.relu)
+        conv_layer = tf.layers.conv2d (inputs = conv_layer, filters = 32, kernel_size = [filter_size, filter_size], padding = "same", activation = tf.nn.relu)
+        pooling = tf.layers.max_pooling2d (inputs = conv_layer, pool_size = [2, 2], strides = 2)
+        dropout = tf.layers.dropout (inputs = pooling, rate = dropout_rate, training = is_training)
+
+        conv_layer = tf.layers.conv2d (inputs = dropout, filters = 64, kernel_size = [filter_size, filter_size], padding = "same", activation = tf.nn.relu)
+        conv_layer = tf.layers.conv2d (inputs = conv_layer, filters = 64, kernel_size = [filter_size, filter_size], padding = "same", activation = tf.nn.relu)
+        pooling = tf.layers.max_pooling2d (inputs = conv_layer, pool_size = [2, 2], strides = 2)
+        dropout = tf.layers.dropout (inputs = pooling, rate = dropout_rate, training = is_training)
+
+        conv_layer = tf.layers.conv2d (inputs = dropout, filters = 128, kernel_size = [3, 3], padding = "same", activation = tf.nn.relu)
+        conv_layer = tf.layers.conv2d (inputs = conv_layer, filters = 128, kernel_size = [3, 3], padding = "same", activation = tf.nn.relu)
         pooling = tf.layers.max_pooling2d (inputs = conv_layer, pool_size = [2, 2], strides = 2)
         pooling_flat = tf.reshape (pooling, [-1, 3 * 3 * 128])
     
-    dense = tf.layers.dense (inputs = pooling_flat, units = units, activation = tf.nn.relu)
-    dropout = tf.layers.dropout (inputs = dense, rate = dropout_rate, training = mode == tf.estimator.ModeKeys.TRAIN)
+    # dense = tf.layers.dense (inputs = pooling_flat, units = units, activation = tf.nn.relu)
+    dropout = tf.layers.dropout (inputs = pooling_flat, rate = dropout_rate, training = is_training)
 
     # Output logits Layer
     # logits = tf.layers.dense(inputs = hidden_layer, units = 10)
@@ -93,22 +103,35 @@ def print_usage ():
     print ("- dropout rate  : rate when applying dropout. 0.0 for doing nothing")
     # print ("- optimizer     : D for GradientDescent, A for Adam")
 
+def validate_depth (depth):
+    if (depth == 3 or depth == 5 or depth == 7):
+        return True
+    return False
+
 if __name__ == '__main__':
     argv = sys.argv[1:]
 
-    if len (sys.argv) != 7:
+    if len (sys.argv) != 6:
         print_usage ()
         sys.exit ()
     
     depth = int (argv[0])
-    units = int (argv[1])
-    batch_size = int (argv[2])
-    learning_rate = float (argv[3])
-    steps = int (argv[4])
-    dropout_rate = float (argv[5])
+    # units = int (argv[1])
+    units = 1024
+    batch_size = int (argv[1])
+    learning_rate = float (argv[2])
+    steps = int (argv[3])
+    # dropout_rate = float (argv[5])
+    dropout_rate = 0.5
+    filter_size = int (argv[4])
+
+    if (validate_depth (depth) == False):
+        print_usage ()
+        sys.exit ()
 
     # Write your dataset path
-    dataset_train = np.load(TRAIN_DATA_PATH)
+    # dataset_train = np.load(TRAIN_DATA_PATH)
+    dataset_train = np.load(TRAIN_DATA_AUGMENTED_PATH)
     dataset_eval =  np.load(EVAL_DATA_PATH)
     test_data =  np.load(TEST_DATA_PATH)
 
@@ -118,19 +141,18 @@ if __name__ == '__main__':
     eval_labels = dataset_eval[:,784].astype(np.int32)
 
     # Save model and checkpoint
-    model_params = {"depth" : depth, "units" : units, "learning_rate" : learning_rate, "dropout_rate" : dropout_rate} 
+    model_params = {"depth" : depth, "units" : units, "learning_rate" : learning_rate, "dropout_rate" : dropout_rate, "filter_size" : filter_size} 
     classifier = tf.estimator.Estimator(model_fn=custom_model_fn, model_dir="./model", params = model_params)
 
     # Set up logging for predictions
     tensors_to_log = {"probabilities": "softmax_tensor"}
     logging_hook = tf.train.LoggingTensorHook(tensors=tensors_to_log, every_n_iter=50)
 
-    # for i in range (5):
-
+# for i in range (10):
     # Train the model. You can train your model with specific batch size and epoches
     train_input = tf.estimator.inputs.numpy_input_fn(x={"x": train_data},
         y=train_labels, batch_size=batch_size, num_epochs=None, shuffle=True)
-    classifier.train(input_fn=train_input, steps=steps, hooks=[logging_hook])
+    classifier.train(input_fn=train_input, steps= (steps), hooks=[logging_hook])
 
     # Eval the model. You can evaluate your trained model with validation data
     eval_input = tf.estimator.inputs.numpy_input_fn(x={"x": eval_data},
